f30882ee999941b22e0c41c1abdf50747934febd
amarmanhala <amar.manhala@gmail.com>
2022-09-18 18:22:24 -0400

    Add notifications

--- src/components/Notifications.js
+++ src/components/Notifications.js
@@ -3,6 +3,6 @@ import styles from './Notifications.module.css'
 
 export default function Notifications() {
   return (
-    <div className={styles.wrapper}><div className={styles.notification}>Notifications</div></div>
+    <div className={styles.wrapper}><div className={styles.notification}><u>Notifications</u>&nbsp;&nbsp;<div className={styles.notificationCount}>10</div></div></div>
   )
 }
